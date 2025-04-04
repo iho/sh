83e1d5c1ebf825cf2e9dbebc41bb4c8b34a1cd2f
5HT <maxim@synrc.com>
2025-04-04 20:22:34 +0300

    wip

--- tests.sh
+++ tests.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 # Simple commands
+
 echo "Simple command"
 echo hello > output.txt
 
 # Pipelines
+
 ls -l | grep foo
 
 # For loop (strict POSIX)
+
 for x in a b c; do
   echo "$x"
 done
 
 # For loop (test case, non-strict POSIX)
+
 for x in a b c do echo $x done
 
 # If statement
+
 if test -z "$var"; then
   echo "empty"
 elif test "$var" = "foo"; then
@@ -25,22 +30,26 @@ else
 fi
 
 # While loop
+
 while test "$i" -lt 5; do
   echo "$i"
 done
 
 # Until loop
+
 until test "$i" -eq 0; do
   echo "$i"
 done
 
 # Case statement
+
 case "$var" in
   foo) echo "Foo match" ;;
   bar) echo "Bar match" ;;
 esac
 
 # Function definition
+
 myfunc() {
   echo "Function called"
 }
